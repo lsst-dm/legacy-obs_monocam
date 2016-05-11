d6d3afb09d90691b3b58d2e602faba26fd454fd4
Simon Krughoff <simon.krughoff@gmail.com>
2016-05-11 13:38:30 -0700

    Fixup for PEP8

--- python/lsst/obs/monocam/monocam.py
+++ python/lsst/obs/monocam/monocam.py
@@ -1,7 +1,7 @@
-# 
+#
 # LSST Data Management System
-# Copyright 2014 LSST Corporation.
-# 
+# Copyright 2016 LSST Corporation.
+#
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
 #
@@ -9,14 +9,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the LSST License Statement and 
-# the GNU General Public License along with this program.  If not, 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 import numpy
@@ -24,6 +24,7 @@ import lsst.afw.cameraGeom as cameraGeom
 import lsst.afw.geom as afwGeom
 from lsst.afw.table import AmpInfoCatalog, AmpInfoTable, LL
 from lsst.afw.cameraGeom.cameraFactory import makeDetector
+
 
 class Monocam(cameraGeom.Camera):
     """The monocam Camera
@@ -37,58 +38,55 @@ class Monocam(cameraGeom.Camera):
     visit: exposure number; this will be provided by the DAQ
     """
     # Taken from fit4_20160413-154303.pdf
-    gain = {
-            (0, 0):3.707220,
-            (1, 0):3.724264,
-            (2, 0):3.758828,
-            (3, 0):3.794040,
-            (4, 0):3.724264,
-            (5, 0):3.776352,
-            (6, 0):3.758828,
-            (7, 0):3.776352,
-            (0, 1):3.724264,
-            (1, 1):3.657009,
-            (2, 1):3.640573,
-            (3, 1):3.624284,
-            (4, 1):3.640573,
-            (5, 1):3.707220,
-            (6, 1):3.673594,
-            (7, 1):3.640573,}
+    gain = {(0, 0): 3.707220,
+            (1, 0): 3.724264,
+            (2, 0): 3.758828,
+            (3, 0): 3.794040,
+            (4, 0): 3.724264,
+            (5, 0): 3.776352,
+            (6, 0): 3.758828,
+            (7, 0): 3.776352,
+            (0, 1): 3.724264,
+            (1, 1): 3.657009,
+            (2, 1): 3.640573,
+            (3, 1): 3.624284,
+            (4, 1): 3.640573,
+            (5, 1): 3.707220,
+            (6, 1): 3.673594,
+            (7, 1): 3.640573}
 
-    readNoise = {
-                 (0, 0):7.105902,
-                 (1, 0):6.860052,
-                 (2, 0):7.387405,
-                 (3, 0):7.222204,
-                 (4, 0):7.250763,
-                 (5, 0):7.315250,
-                 (6, 0):7.104838,
-                 (7, 0):7.272336,
-                 (0, 1):8.485139,
-                 (1, 1):8.022778,
-                 (2, 1):8.157399,
-                 (3, 1):8.021112,
-                 (4, 1):8.015486,
-                 (5, 1):7.928829,
-                 (6, 1):16.031720,
-                 (7, 1):7.938155,}
-
+    readNoise = {(0, 0): 7.105902,
+                 (1, 0): 6.860052,
+                 (2, 0): 7.387405,
+                 (3, 0): 7.222204,
+                 (4, 0): 7.250763,
+                 (5, 0): 7.315250,
+                 (6, 0): 7.104838,
+                 (7, 0): 7.272336,
+                 (0, 1): 8.485139,
+                 (1, 1): 8.022778,
+                 (2, 1): 8.157399,
+                 (3, 1): 8.021112,
+                 (4, 1): 8.015486,
+                 (5, 1): 7.928829,
+                 (6, 1): 16.031720,
+                 (7, 1): 7.938155}
 
     def __init__(self):
         """Construct a TestCamera
         """
-        plateScale = afwGeom.Angle(13.55, afwGeom.arcseconds) # plate scale, in angle on sky/mm
-        radialDistortion = 0. # radial distortion in mm/rad^2
+        plateScale = afwGeom.Angle(13.55, afwGeom.arcseconds)  # plate scale, in angle on sky/mm
+        radialDistortion = 0.  # radial distortion in mm/rad^2
         radialCoeff = numpy.array((0.0, 1.0, 0.0, radialDistortion)) / plateScale.asRadians()
         focalPlaneToPupil = afwGeom.RadialXYTransform(radialCoeff)
         pupilToFocalPlane = afwGeom.InvertedXYTransform(focalPlaneToPupil)
         cameraTransformMap = cameraGeom.CameraTransformMap(cameraGeom.FOCAL_PLANE,
-            {cameraGeom.PUPIL: pupilToFocalPlane})
+                                                           {cameraGeom.PUPIL: pupilToFocalPlane})
         detectorList = self._makeDetectorList(pupilToFocalPlane, plateScale)
         cameraGeom.Camera.__init__(self, "monocam", detectorList, cameraTransformMap)
 
     def _makeDetectorList(self, focalPlaneToPupil, plateScale):
-        """Make a list of detectors
+        """!Make a list of detectors
 
         @param[in] focalPlaneToPupil  lsst.afw.geom.XYTransform from FOCAL_PLANE to PUPIL coordinates
         @param[in] plateScale  plate scale, in angle on sky/mm
@@ -99,12 +97,12 @@ class Monocam(cameraGeom.Camera):
         for detectorConfig in detectorConfigList:
             ampInfoCatalog = self._makeAmpInfoCatalog()
             detector = makeDetector(detectorConfig, ampInfoCatalog, focalPlaneToPupil,
-                plateScale.asArcseconds())
+                                    plateScale.asArcseconds())
             detectorList.append(detector)
         return detectorList
 
     def _makeDetectorConfigList(self):
-        """Make a list of detector configs
+        """!Make a list of detector configs
 
         @return a list of detector configs (lsst.afw.cameraGeom.DetectorConfig)
         """
@@ -119,8 +117,8 @@ class Monocam(cameraGeom.Camera):
         detector0Config.bbox_x1 = 4095
         detector0Config.bbox_y0 = 0
         detector0Config.bbox_y1 = 4003
-        detector0Config.pixelSize_x = 0.01 # in mm
-        detector0Config.pixelSize_y = 0.01 # in mm
+        detector0Config.pixelSize_x = 0.01  # in mm
+        detector0Config.pixelSize_y = 0.01  # in mm
         detector0Config.transformDict.nativeSys = 'Pixels'
         detector0Config.transformDict.transforms = None
         detector0Config.refpos_x = 2001.5
@@ -129,7 +127,7 @@ class Monocam(cameraGeom.Camera):
         detector0Config.offset_y = 0.0
         detector0Config.transposeDetector = False
         detector0Config.pitchDeg = 0.0
-        detector0Config.yawDeg = 0.0 # this is where chip rotation goes in.
+        detector0Config.yawDeg = 0.0  # this is where chip rotation goes in.
         detector0Config.rollDeg = 0.0
         return [detector0Config]
 
@@ -137,15 +135,15 @@ class Monocam(cameraGeom.Camera):
         """Construct an amplifier info catalog
         """
         # Much of this will need to be filled in when we know it.
-        xDataExtent = 512 # trimmed
+        xDataExtent = 512  # trimmed
         yDataExtent = 2002
 
-        extended = 10 # extended register
-        h_overscan = 22 # number of overscan in x
-        v_overscan = 46 # number of overscan in y
+        extended = 10  # extended register
+        h_overscan = 22  # number of overscan in x
+        v_overscan = 46  # number of overscan in y
 
         xRawExtent = extended + h_overscan + xDataExtent
-        yRawExtent = v_overscan + yDataExtent # no prescan in vertical
+        yRawExtent = v_overscan + yDataExtent  # no prescan in vertical
 
         saturation = 65535
         # Linearity correction is still under discussion, so this is a placeholder.
@@ -166,19 +164,19 @@ class Monocam(cameraGeom.Camera):
             for ampX in xrange(8):
                 record = ampCatalog.addNew()
                 record.setName("%d%d" % (ampX, ampY))
-                
+
                 if bool(ampY):
                     record.setBBox(afwGeom.Box2I(
-                            afwGeom.Point2I(ampX * xDataExtent, ampY * yDataExtent),
-                                            afwGeom.Extent2I(xDataExtent, yDataExtent),
+                        afwGeom.Point2I(ampX*xDataExtent, ampY*yDataExtent),
+                        afwGeom.Extent2I(xDataExtent, yDataExtent),
                     ))
                 else:
                     record.setBBox(afwGeom.Box2I(
-                            afwGeom.Point2I((7-ampX) * xDataExtent, ampY * yDataExtent),
-                                            afwGeom.Extent2I(xDataExtent, yDataExtent),
+                        afwGeom.Point2I((7 - ampX)*xDataExtent, ampY*yDataExtent),
+                        afwGeom.Extent2I(xDataExtent, yDataExtent),
                     ))
 
-                readCorner = LL # in raw frames; always LL because raws are in amp coords
+                readCorner = LL  # in raw frames; always LL because raws are in amp coords
                 # bias region
                 x0Bias = extended + xDataExtent
                 y0Data = 0
